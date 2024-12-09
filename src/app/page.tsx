4773eceb3a8b4a38c7ee275c6715076c0409dc68
Shagufta-Naz-123 <shagufta590051@gmail.com>
2024-12-09 13:27:57 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+
+
+import Categories from "./Components/Categories";
+import CompanyLogo from "./Components/companyLogo";
+
+import Hero from "./Components/Hero";
+import HotProduct from "./Components/hotProducts";
+import OurProduct from "./Components/ourProduct";
+
+
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <CompanyLogo />
+      
+
+      <Categories />
+      <HotProduct />
+      <OurProduct /> 
+    </>
+  );
+}
